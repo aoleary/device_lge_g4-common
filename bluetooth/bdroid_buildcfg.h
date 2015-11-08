43556081f52c38e0245dd2f37bd71e5cc8983a24
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-11-08 20:37:18 +0000

    g4: bluetooth: enable and force wbs, enable ble peripheral mode
    Change-Id: I680156f588b12800de254be73833b5563b62a2d0

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,6 +18,11 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME "LG G4"
-#define BTA_DISABLE_DELAY 100 /* in milliseconds */
+
+#define BLE_VND_INCLUDED TRUE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+
+#define BTM_WBS_INCLUDED TRUE /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED TRUE /* Use WBS */
 
 #endif
