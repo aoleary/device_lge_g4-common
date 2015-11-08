69235f657b0653ff7b59ff30e121877f7b13a94c
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-11-08 10:58:54 +0000

    g4: init: additional changes from v20a
    Change-Id: Ia04a0b6fdf70c5dbc98ea344c779fa53b7fc407a

--- rootdir/etc/init.class_main.sh
+++ rootdir/etc/init.class_main.sh
@@ -26,6 +26,25 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+start_copying_prebuilt_qcril_db()
+{
+    if [ -f /system/vendor/qcril.db -a ! -f /data/misc/radio/qcril.db ]; then
+        cp /system/vendor/qcril.db /data/misc/radio/qcril.db
+        chown -h radio.radio /data/misc/radio/qcril.db
+    fi
+}
+start_copying_prebuilt_qcril_db
+
+#
+# Make modem config folder and copy firmware config to that folder
+#
+rm -rf /data/misc/radio/modem_config
+mkdir /data/misc/radio/modem_config
+chmod 660 /data/misc/radio/modem_config
+cp -r /firmware/image/modem_pr/mbn_ota/* /data/misc/radio/modem_config
+chown -hR radio.radio /data/misc/radio/modem_config
+echo 1 > /data/misc/radio/copy_complete
+
 # grep the modem partition for baseband version and set it
 setprop gsm.version.baseband `strings /firmware/image/modem.b13 | grep "\-M8992" | head -1`
 
