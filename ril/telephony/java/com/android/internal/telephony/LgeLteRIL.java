8f57d0b5a03e03a3ea89aa26f7774eb33c8b4f15
codeworkx <codeworkx@cyanogenmod.org>
2016-02-09 18:58:20 +0100

    g4: ril: send fake RAT on manual network selection
    Change-Id: Ic9456b860159944d7681040d071be5395bdc5124

--- ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/LgeLteRIL.java
@@ -101,7 +101,7 @@ public class LgeLteRIL extends RIL implements CommandsInterface {
 
         rr.mParcel.writeInt(2);
         rr.mParcel.writeString(operatorNumeric);
-        rr.mParcel.writeString("NOCHANGE");
+        rr.mParcel.writeString("2"); // NOCHANGE
 
         send(rr);
     }
