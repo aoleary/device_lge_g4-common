329d9f06cff6347498b9fbe64df4fbcce72d88e0
Diogo Ferreira <defer@cyngn.com>
2016-04-26 17:31:11 +0100

    g4-common: Tune Dalvik VM memory parameters a bit more
    dalvik.vm.heapminfree: 4m -> 512kb, reduce the minimum reclaim value,
    this will prevent stalls in resource-constrained scenarios.
    
    Change-Id: I6b3bacfc061f7d2ec6cce57738a79dc5861894be

--- system_prop.mk
+++ system_prop.mk
@@ -110,8 +110,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.7 \
-    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512K \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.dex2oat64.enabled=true
 
