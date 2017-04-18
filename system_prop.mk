d5353d4710dd87d24f8bfa390ab5c53063cf0085
Aren Clegg <aclegg2011@gmail.com>
2017-04-18 15:19:55 -0700

    g4-common: Enable camera HAL3
    Helps with Google Camera (GCam)

--- system_prop.mk
+++ system_prop.mk
@@ -53,6 +53,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.camera.facialproc=false \
     ro.qc.sdk.gestures.camera=false \
     camera2.portability.force_api=1 \
+    persist.camera.video.ubwc=0 \
+    persist.camera.HAL3.enabled=1 \
     ro.factorytest=0
 
 # DRM
