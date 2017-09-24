e2c63087ed495a4ea32a01914ce81c3609d5d64d
Michael Bestas <mkbestas@lineageos.org>
2017-09-24 20:38:07 +0300

    g4-common: gps: Cleanup makefiles and remove unused loc_api
    Change-Id: Iea3d329dd5f04598d1994eec1f626d9b95b08502

--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,6 +1,3 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -39,7 +36,7 @@ LOCAL_CFLAGS += \
     -Wno-unused-variable
 
 ifeq ($(TARGET_BUILD_VARIANT),user)
-   LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
+    LOCAL_CFLAGS += -DTARGET_BUILD_VARIANT_USER
 endif
 
 LOCAL_LDFLAGS += -Wl,--export-dynamic
@@ -50,28 +47,25 @@ LOCAL_C_INCLUDES:= \
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
-   loc_log.h \
-   loc_cfg.h \
-   log_util.h \
-   linked_list.h \
-   msg_q.h \
-   MsgTask.h \
-   LocHeap.h \
-   LocThread.h \
-   LocTimer.h \
-   loc_target.h \
-   loc_timer.h \
-   LocSharedLock.h \
-   platform_lib_abstractions/platform_lib_includes.h \
-   platform_lib_abstractions/platform_lib_time.h \
-   platform_lib_abstractions/platform_lib_macros.h \
-   loc_misc_utils.h
+    loc_log.h \
+    loc_cfg.h \
+    log_util.h \
+    linked_list.h \
+    msg_q.h \
+    MsgTask.h \
+    LocHeap.h \
+    LocThread.h \
+    LocTimer.h \
+    loc_target.h \
+    loc_timer.h \
+    LocSharedLock.h \
+    platform_lib_abstractions/platform_lib_includes.h \
+    platform_lib_abstractions/platform_lib_time.h \
+    platform_lib_abstractions/platform_lib_macros.h \
+    loc_misc_utils.h
 
 LOCAL_MODULE := libgps.utils
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
-
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
