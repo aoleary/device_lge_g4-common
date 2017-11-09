b3b59f3ec63d6a17d5a976cf041e7ea9991241f2
Erwan Leboucher <erwanleboucher@gmail.com>
2017-11-09 09:30:10 +0100

    Update camera.mk

--- product/camera.mk
+++ product/camera.mk
@@ -1,7 +1,7 @@
 PRODUCT_PACKAGES += \
     Snap
 
-PRODUCT_PACKAGES += \
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
