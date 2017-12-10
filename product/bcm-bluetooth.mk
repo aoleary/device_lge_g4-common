aa9ceb68649dbacb70bb45093b9af53c35047c5c
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-10 19:31:12 +0100

    g4: remove libdrmclearkeyplugin, fix bcm firmware
    Change-Id: Ib54ac8637916709dfab48bcfb25191aea6009ca2

--- product/bcm-bluetooth.mk
+++ product/bcm-bluetooth.mk
@@ -9,7 +9,6 @@ PRODUCT_PACKAGES += \
     hwaddrs \
     libprotobuf-cpp-full
 
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
