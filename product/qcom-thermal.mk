df781001052f28c5e7aa0a416ffe80740ced08a4
pavlaras <theodoroupaulos@gmail.com>
2017-12-15 06:19:58 +0100

    thermal-engine: Fix qcom thermal engine error

--- product/qcom-thermal.mk
+++ product/qcom-thermal.mk
@@ -4,4 +4,4 @@ PRODUCT_PACKAGES += \
     thermal.msm8992
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8992.conf
