e0f14971be1afa541cce8e556e07885ed931dfe9
Bruno Martins <bgcngm@gmail.com>
2017-12-24 20:02:44 -0300

    g4: Build graphics allocator service

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -1,7 +1,8 @@
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     copybit.msm8992 \
     gralloc.msm8992 \
