f621f10e94e1a33a02645b90d61f66328433d778
pavlaras <theodoroupaulos@gmail.com>
2017-12-30 08:59:50 +0100

    g4: camera: sensor: Add camera hal sensors tof
    Change-Id: Icb3b31d252ba16d8cbac485fc072f66dfae4a800

--- product/camera.mk
+++ product/camera.mk
@@ -1,7 +1,8 @@
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    sensors.hal.tof
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
