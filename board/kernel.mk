ca55118e1161ffe62c5e8eddc3ad2d32eadd9f89
pavlaras <theodoroupaulos@gmail.com>
2018-01-01 18:28:15 +0100

    g4: selinux set to enforcing
    Change-Id: Id3f0c45631bcce751335b77031d1ab38fdaef011

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,4 +1,4 @@
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.selinux=permissive androidboot.console=ttyHSL0 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x37 boot_cpus=0-5
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
