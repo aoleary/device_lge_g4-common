703a1ad08c68dea5085077f34b58b601ad41148c
pavlaras <theodoroupaulos@gmail.com>
2018-01-08 09:14:02 +0100

    g4: camera: Add hal camera.msm8992
    Change-Id: I14dd5a9d2268b2a9d050f125f812f98e31f3e02f

--- product/camera.mk
+++ product/camera.mk
@@ -2,6 +2,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
+    camera.msm8992 \
     sensors.hal.tof \
     libmm-qcamera
 
