b9b98aac3c9fd3a0968ead072acc8a991aaff4d8
pavlaras <theodoroupaulos@gmail.com>
2018-01-09 10:20:38 +0100

    g4: create missing socket needed for ril and allow ril to write at it     *fix error: QcrilMsgTunnelSocket:         Couldn't find 'qmux_radio/rild_oem0' socket; retrying after timeout     *symling diag to diag_lge to unbreak lg proprietary tools         symlink will fix the error: /dev/diag_lge -1 ENOENT (No such file or directory)
    Change-Id: I1b605cdb6f3362bfb9eb14e93728d429ffc434dd

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -44,4 +44,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml 
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    $(LOCAL_PATH)/configs/qcril.db:system/etc/motorola/qcril.db
