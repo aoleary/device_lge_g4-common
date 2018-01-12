7cbc861b585234612a28e0af39f91c80f8f8208f
pavlaras <theodoroupaulos@gmail.com>
2018-01-12 14:59:29 +0100

    g4: sec_config: Bring back sec_config to fix 3rd party apps access to gps and several sensors
    Change-Id: I6e3d43c2929b470fc7f9875472f248c5a40f17d1

--- product/qcom-misc.mk
+++ product/qcom-misc.mk
@@ -1,4 +1,5 @@
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 PRODUCT_PACKAGES += \
