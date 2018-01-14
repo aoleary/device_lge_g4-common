58c9cfd8c42d128251716463b9fc641d1ab658ff
pavlaras <theodoroupaulos@gmail.com>
2018-01-14 18:21:29 +0100

    g4: Fix more ril errors from strace     *add missing qmi_fw.conf and ctbk_val.cfg    *init.qcom.rc: create rild needed folders and files   *create missing devices in soc and grant access to ril to read from them
    Change-Id: If894f369a0bf89cb6c1a3bdafa61611f819ed6bd

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -62,3 +62,19 @@ if [ "$diag_extra" == "" ]; then
 	setprop persist.sys.usb.config.extra none
 fi
 
+#
+# Check ESOC for external MDM
+#
+# Note: currently only a single MDM is supported
+#
+if [ -d /sys/bus/esoc/devices ]; then
+for f in /sys/bus/esoc/devices/*; do
+    if [ -d $f ]; then
+        esoc_name=`cat $f/esoc_name`
+        if [ "$esoc_name" = "MDM9x25" -o "$esoc_name" = "MDM9x35" ]; then
+            esoc_link=`cat $f/esoc_link`
+            break
+        fi
+    fi
+done
+fi
