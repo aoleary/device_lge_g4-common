19df70672103b3adcd5445f63217346bfaff2b5d
pavlaras <theodoroupaulos@gmail.com>
2018-01-17 08:07:39 +0100

    g4:audio: Add aanc_tuning_mixer.txt sepolicy: allow audio hal to create acdb data files init.qcom.rc: Test create ril sockets as files
    Change-Id: Ib57e29590573bc3599b3912880a484dc7083733f

--- product/audio.mk
+++ product/audio.mk
@@ -14,10 +14,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_qvr.xml:system/etc/mixer_paths_qvr.xml \
     $(LOCAL_PATH)/audio/mixer_paths_skuk.xml:system/etc/mixer_paths_skuk.xml \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:system/etc/mixer_paths_tavil.xml \
+    $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml 
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml
 
 # fluence multi-mic solution
 PRODUCT_PROPERTY_OVERRIDES += \
