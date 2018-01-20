93004ec2d5dabd3070a811572e74b708671cf8e6
pavlaras <theodoroupaulos@gmail.com>
2018-01-20 02:33:27 +0100

    g4: camera props: force api1
    Change-Id: I70840b9c3bb952ea0d52d93939e7287d7302054d

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,3 +1,4 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.camera.facialproc=false \
-    ro.qc.sdk.gestures.camera=false
+    ro.qc.sdk.gestures.camera=false \
+    camera2.portability.force_api=1
