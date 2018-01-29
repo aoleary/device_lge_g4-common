ee049fa56aa631895da61ba9ab97560cee034fc1
pavlaras <theodoroupaulos@gmail.com>
2018-01-29 22:18:28 +0100

    g4: small corrections setprop ro.factorytest=0 and time_daemon group add diag_lge
    Change-Id: I89611db8eb439b7c6b2a5be42f26d2efe0b20deb

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,4 +1,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.camera.facialproc=false \
     ro.qc.sdk.gestures.camera=false \
-    camera2.portability.force_api=1
+    camera2.portability.force_api=1 \
+    ro.factorytest=0
