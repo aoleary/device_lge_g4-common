3e6387e23c851988ed442f7c594a7ee6561ed1b4
Simao Gomes Viana <xdevs23@outlook.com>
2018-02-11 18:21:48 +0100

    g4-common: Tweak fling velocity system props
    * And remove either deprecated or useless properties
    
    Change-Id: I755a802e45fd07733e7efd42ad967f5a91190bb0
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -117,12 +117,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Fling Velocity
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min.fling_velocity=8000 \
-    ro.max.fling_velocity=12000 \
-    ro.min_pointer_dur=8 \
-    persist.sys.scrollingcache=3 \
-    touch.presure.scale=0.001 \
-    windowsmgr.max_events_per_sec=150
+    ro.min.fling_velocity=160 \
+    ro.max.fling_velocity=20000
 
 # FRP
 PRODUCT_PROPERTY_OVERRIDES += \
