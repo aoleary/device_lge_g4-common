ef6ff05da45574a7bf933a24f5ac3aae9fa2afba
pavlaras <paul@cypheros.co>
2018-02-20 04:57:59 +0100

    g4: Add health hals
    To Successfully initiate health check
    
    Change-Id: Ifdd2328c28d6a667a396131cef96212b50fa0e64

--- product/power.mk
+++ product/power.mk
@@ -1,3 +1,6 @@
 PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service \
     android.hardware.power@1.0-impl \
     power.msm8992
