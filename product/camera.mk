75c0f8e8b9d14a40ba077ca12fdfd51af2550037
kessaras <theodoroupaulos@gmail.com>
2018-02-23 09:53:12 +0200

    g4: Initial bringup OSS camera HAL
    Change-Id: I0b3b7ba331322e0b7ab865dc1b3adad7548cc040

--- product/camera.mk
+++ product/camera.mk
@@ -3,6 +3,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     camera.msm8992 \
+    libqomx_core \
     libmm-qcamera
 
 PRODUCT_COPY_FILES += \
