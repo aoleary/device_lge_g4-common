6e6ed2a1f5ac1ae7a9e22091f3c3094eef606f24
pavlaras <paul@cypheros.co>
2018-05-22 14:28:44 +0200

    g4: init: Add service chcon_keystore
    Change-Id: Ia09c32f74f0cf77203dd944bad0a60607ddea4d6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -124,3 +124,11 @@ LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/init.qcom.bt.sh
 LOCAL_MODULE_PATH   := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE        := init.recovery.m1_chcon_keystore.sh
+LOCAL_MODULE_TAGS   := optional eng
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := etc/init.recovery.m1_chcon_keystore.sh
+LOCAL_MODULE_PATH   := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
---
+++ rootdir/etc/init.recovery.m1_chcon_keystore.sh
@@ -0,0 +1,3 @@
+#!system/bin/sh
+
+chcon -R u:object_r:system_data_file:s0 /data/misc/keystore
