325c5e32aaf35ca66c4363b8b55346c4e92e5d5e
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    g4-common: Remove camera.device@1.0/3.2-impl from PRODUCT_PACKAGES
    * camera.device@1.0/3.2-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: Id6da9c3ff7ecaa0f6378c8bf4de0e2302f9497fc

--- g4.mk
+++ g4.mk
@@ -87,8 +87,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
     camera.device@3.4-external-impl \
     camera.device@3.5-external-impl \
     android.hardware.camera.provider@2.4-impl \
