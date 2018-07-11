919acc70f363adfd13795bfa997e96ecb2f7e138
steadfasterX <steadfastX@boun.cr>
2018-07-11 19:24:34 +0200

    g4: charger: fix charger mode
    animation based on:
    https://github.com/Aaahh/Battery-Images-Replacer
    
    only battery_scale.png and battery_fail.png must be copied
    to the ramdisk.
    the battery_#.png are to create battery_scale.png by
    create_chargeranim.sh
    HINT: more then 6 frames (so 0-5 battery images) will
    destroy the scaling!
    
    Change-Id: I92f8ab57ca79f7f18ca8ad4b21befb6950bdfe8a

--- product/power.mk
+++ product/power.mk
@@ -4,3 +4,8 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-service \
     android.hardware.power@1.0-impl \
     power.msm8992
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/ramdisk/sbin/healthd:root/sbin/healthd \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/battery_scale.png:root/res/images/charger/battery_scale.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png
