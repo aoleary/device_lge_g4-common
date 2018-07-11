4e422f431fed3c3feec1b00a1423fd561d9619c7
steadfasterX <steadfastX@boun.cr>
2018-07-11 11:01:06 +0200

    g4: charger: add good old healthd
    taken from LOS 14.1 as the Oreo way isn't working for
    offline charging
    
    Change-Id: I60fd447b76c03e9f3da71e1b51243ad4185ab406

---
+++ charger.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/ramdisk/sbin/healthd:root/sbin/healthd \
+        $(LOCAL_PATH)/ramdisk/res/images/font_log.png:root/res/images/font_log.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/cm_battery_scale.png:root/res/images/charger/cm_battery_scale.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/battery_scale.png:root/res/images/charger/battery_scale.png \
+        $(LOCAL_PATH)/ramdisk/res/images/charger/battery_fail.png:root/res/images/charger/battery_fail.png
