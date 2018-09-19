587d39c5dd8acd609db05905fb892fe10b15e4bb
pavlaras <paul@cypheros.co>
2018-09-19 09:10:41 +0200

    g4: power hal: Remove TARGET_TAP_TO_WAKE_NODE
    It was never there to begin with
    
    Change-Id: I0053fa6c22b6026e5f1450cf4041fe8966ffc894

--- board/power.mk
+++ board/power.mk
@@ -3,6 +3,4 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGER_ENABLE_SUSPEND := false
 BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
 
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap2wake"
-
 TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
