6fe96e4f1a72ae8598264b1c03ea4b779d07a477
pavlaras <paul@cypheros.co>
2018-10-01 22:04:46 +0200

    g4: camera: Remove call to build camera hal. We use prebuilt
    Change-Id: I990787c83c9da125657d29eb9eef1480abac3472

--- product/camera.mk
+++ product/camera.mk
@@ -2,7 +2,6 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    camera.msm8992 \
     libqomx_core \
     libmm-qcamera
 
