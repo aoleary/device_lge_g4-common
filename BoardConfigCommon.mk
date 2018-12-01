f2264b26301b9e824495481e1e343cc49515e2a9
Aleksander Gencel <johnnylittleplanet@gmail.com>
2018-12-01 11:36:05 +0100

    g4-common: set wificountrycode to US
    * enables 5GHz wifi hotspot
    * this was suggested by one of our users on XDA which seems to work
    
    * Work for the G3 for years, so hopefully it works for the G4 also
    
    Change-Id: Iac599ad687590dd0c7e861112d2880f9e09f93d2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -153,7 +153,7 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.selinux=enforcing
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.wificountrycode=us
 BOARD_KERNEL_CMDLINE += user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x37 boot_cpus=0-5 lge_monitor_thermal.enable=1 fakebattery=enable
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
