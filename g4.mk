f8a3d8d986f51adb0266dd6d463a1f25fd1313ca
Paul Keith <javelinanddart@gmail.com>
2019-02-07 13:12:30 -0600

    g4-common: Build LiveDisplay 2.0 via sysfs service
    * Add supported interfaces to manifest while we're at it
    
    Edit: It's intended to not update to vendor.lineage.livedisplay@1.0-service-legacymm
    because the provided feature IPictureAdjustment isn't supported by our device because of the lack
    of suitable display calibration data which is needed by mm-pp-daemon. Without that the error messages are as follows:
    
    03-13 18:16:42.199  7186  7186 I vendor.lineage.livedisplay@2.0-service-legacymm: LiveDisplay HAL service is starting.
    03-13 18:16:42.202  7186  7186 D DISP_API: disp_api_get_num_display_modes.
    03-13 18:16:42.203  6058  7187 D PPDaemon: PU Status from pu node: 0
    03-13 18:16:42.203  6058  7187 D modemanager: Mode Manager could not locate/access OEM Calibration file
    03-13 18:16:42.204  6058  7187 E PPDaemon: int DaemonContext::LoadQdcmLibrary(): Failed creating mode manager instance
    03-13 18:16:42.204  7186  7186 D DISP_API: disp_api_get_pa_range.
    03-13 18:16:42.225  6058  7187 D PPDaemon: PU Status from pu node: 0
    03-13 18:16:42.226  6058  7187 E PPDaemon: Failed to process the command!
    03-13 18:16:42.226  6058  7187 D PPDaemon: PU Status from pu node: 0
    03-13 18:16:42.227  6058  7187 D modemanager: Mode Manager could not locate/access OEM Calibration file
    03-13 18:16:42.227  6058  7187 E PPDaemon: int DaemonContext::LoadQdcmLibrary(): Failed creating mode manager instance
    03-13 18:16:42.227  7186  7186 E DISP_API: disp_api_get_pa_range: Sending command failed for getting range
    03-13 18:16:42.228  7186  7186 E vendor.lineage.livedisplay@2.0-service-legacymm: LiveDisplay HAL service is shutting down.
    03-13 18:16:42.248  6058  7187 D PPDaemon: PU Status from pu node: 0
    03-13 18:16:42.249  6058  7187 E PPDaemon: Failed to process the command!
    03-13 18:16:42.312  6154  6177 W ServiceManagement: Waited one second for vendor.lineage.livedisplay@2.0::IPictureAdjustment/default. Waiting another...
    
    Change-Id: Iaad13fcdfb603d144599133339f9c0c6a0d1c66a

--- g4.mk
+++ g4.mk
@@ -192,7 +192,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # MP-CTL
 PRODUCT_PACKAGES += \
