73783557394765f81cb14462dab6cda53fabca18
steadfasterX <steadfasterX@gmail.com>
2019-04-29 16:33:48 +0200

    g4-common: kernel: selinux permissive (fixed)
    Change-Id: I7b2ad5dce91c157727e81db5067efb4ab1a692f7

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x37 boot_cpus=0-5 lge_monitor_thermal.enable=1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
