3b314a92920814e5e78114fe6345ec83eab724b9
Michael Bestas <mkbestas@lineageos.org>
2019-05-01 14:55:46 +0200

    g4-common: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- product/power.mk
+++ product/power.mk
@@ -2,7 +2,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service \
-    android.hardware.power@1.0-impl \
+    android.hardware.power@1.1-service-qti \
     power.msm8992
 
 PRODUCT_COPY_FILES += \
