5c59b21aa911ea3126c0bacb24f6fa93b953ec3e
Artem Borisov <dedsa2002@gmail.com>
2019-05-12 21:13:39 +0530

    g4-common: Disable triple buffering
    * Historically, triple buffering was used to workaround missed VSYNC in GPU composition usecases on devices with high resolution panels.
      As a downside, an additional frame buffer increased input latency.
      Nowadays, most GPUs are fast enough to fit the VSYNC period, rendering triple framebuffers useless.

--- system_prop.mk
+++ system_prop.mk
@@ -227,7 +227,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Properties for Surfaceflinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_virtual_display_dimension=2048 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=1 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.use_color_management=false \
     ro.surface_flinger.has_HDR_display=false \
