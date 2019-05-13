7d1652a0461d9d17d58d7979c1135aece84d9651
steadfasterX <steadfasterX@gmail.com>
2019-05-13 13:20:51 +0200

    g4-common: torch: workaround completely adapted
     * fixed led path
     * fixed file contexts
     * fully implemented flashlight_app as a seapp policy!
     * fixed build error on pie (requires LOCAL_PRIVATE_PLATFORM_APIS to be set)
     * removing kenzo related stuff
    
    Now the flashlight will work without root and even when selinux=enforcing
    
    Change-Id: I3dc44990064d14e1945d8929d2d6abfa03c6e96e

--- flashlight/src/com/abhi/flashlight/Constants.java
+++ flashlight/src/com/abhi/flashlight/Constants.java
@@ -2,10 +2,11 @@ package com.abhi.flashlight;
 
 /**
  * Created by men_in_black007 on 27/1/17.
- */
+ * Adapted to LG G4 by steadfasterX.
+**/
 
 public class Constants {
 
-    public static final String PATH = "/sys/devices/soc.0/qpnp-flash-led-23/leds/flashlight/brightness";
+    public static final String PATH = "/sys/devices/soc.0/qpnp-flash-led-26/leds/led:torch_1/brightness";
 
 }
