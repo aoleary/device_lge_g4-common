4c3c65c9b627c01676bb58f21e3f80d13fb7b3ba
Lakshay Garg <lakshaygarg.in@gmail.com>
2019-05-19 07:44:42 +0000

    g4-common: Enable Camera EIS
    Signed-off-by: Lakshay Garg <lakshaygarg.in@gmail.com>

--- g4.mk
+++ g4.mk
@@ -76,6 +76,13 @@ PRODUCT_PACKAGES += \
     libqomx_core \
     Camera2
 
+# Enable camera EIS
+# eis.enable: enables electronic image stabilization
+# is_type: sets image stabilization type
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.eis.enable=1 \
+    persist.camera.is_type=4
+
 # rootless torch tile workaround
 PRODUCT_PACKAGES += \
     Flashlight
