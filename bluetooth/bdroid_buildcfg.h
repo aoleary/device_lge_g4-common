41499051222ba13dfb5bb05c3c67d8e7d1619cc2
steadfasterX <steadfasterX@gmail.com>
2019-06-27 07:37:46 +0200

    g4-common: bluetooth: fix BT voice (WBS headsets)
     * Do not use WBS when BTHW_FW_EXTENDED_CONFIGURATION = TRUE (vnd conf)
     * When set to true and your headset supports WBS it will
     * just be silence on both sites - at least when WBS does actually turn on
     * which is not the case for all WBS enabled headsets.
    
    Change-Id: Iffbfabfea38b60e464381ce7f224fcb7cc646e33

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -31,7 +31,13 @@
 
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
-#define BTIF_HF_WBS_PREFERRED TRUE /* Use WBS */
+/*
+ * Do not use WBS when BTHW_FW_EXTENDED_CONFIGURATION = TRUE (vnd conf)
+ * When set to true and your headset supports WBS it will
+ * just be silence on both sites - at least when WBS does actually turn on 
+ * which is not the case for all WBS enabled headsets.
+*/
+#define BTIF_HF_WBS_PREFERRED FALSE /* Do not use WBS */
 
 #define BT_CLEAN_TURN_ON_DISABLED 1
 #endif
