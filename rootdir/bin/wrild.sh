c071bab2c27470dab4b566ae0f5251fbfc830320
steadfasterX <steadfasterX@gmail.com>
2019-07-11 20:00:56 +0200

    g4-common: wrild: detect boot, act more sensitive
    wrild.sh will now wait until a boot completely finished (no time
    limit), delays a first restart of RIL and in general will wait
    longer when doing failure handlings.
    this was required as rild do not crash anymore and so we need wrild
    for rare cases only.

--- rootdir/bin/wrild.sh
+++ rootdir/bin/wrild.sh
@@ -19,18 +19,25 @@ F_LOG(){
 F_RILCHK(){
     CURSTATE=$(getprop gsm.sim.state)
     CUROPER=$(getprop gsm.sim.operator.numeric)
+    DBOOTED=$(getprop sys.boot_completed)
+
+    F_LOG "i" "sys.boot_completed >$DBOOTED<"
     F_LOG "i" "gsm.sim.state >$CURSTATE<"
     F_LOG "i" "gsm.sim.operator.numeric >$CUROPER<"
 
     if [ "$CURSTATE" == "READY" ]; then
         echo 0
+    elif [ "$DBOOTED" != "1" ];then
+	echo 7
     elif  [ "$CURSTATE" == "PIN_REQUIRED" ]; then
         echo 9
     elif [ "$CURSTATE" == "LOADED" ] && [ -z "$CUROPER" ];then
+        sleep 20
         echo 1
     elif [ "$CURSTATE" == "LOADED" ] && [ ! -z "$CUROPER" ];then
         echo 0
     else
+        sleep 20
         echo 1
     fi
 }
@@ -43,22 +50,25 @@ while [ "$REQRESTART" -ne 0 ];do
     # this will restart RIL not on the first but every second run only (which should be safe) and
     # let the user enough time to enter the PIN if the prompt appears
     if [ "$REQRESTART" -eq 9 ]&&[ $PRTRIGGER -eq 0 ];then
-        F_LOG i "PIN_REQUIRED detected. waiting 10s for user input.." && sleep 10
+        F_LOG i "PIN_REQUIRED detected. waiting 40s for user input.." && sleep 40
         PRTRIGGER=1
+    elif [ "$REQRESTART" -eq 7 ];then
+	F_LOG i "Boot (still) in progress ... hanging around for 20s ..." && sleep 20
+	x=1
     elif [ "$REQRESTART" -eq 1 ];then
         F_LOG w "RIL restart - try $x of $MAXRET"
         stop real-ril-daemon
         sleep 1
         start real-ril-daemon
         F_LOG w "restarted RIL daemon as REQRESTART was set to >$REQRESTART<"
-        sleep 20
+        sleep 40
         PRTRIGGER=0
     elif [ "$REQRESTART" -eq 9 ] ;then
-        F_LOG i "PIN_REQUIRED detected. waiting another 20s for user input.." && sleep 20
+        F_LOG i "PIN_REQUIRED detected. waiting another minute for user input.." && sleep 60
     elif [ "$REQRESTART" -eq 0 ] ;then
         F_LOG i "no restart required. RILD seems to work properly already." && break
     else
-        F_LOG e "unusual state detected . waiting 10s and will try again .." && sleep 20
+        F_LOG e "unusual state detected . waiting 20s and will try again .." && sleep 20
     fi
     x=$((x + 1))
     if [[ $x -eq $MAXRET ]];then
