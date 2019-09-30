7aa2dc14739e1019647f8580f180e8205e34ac27
aoleary <seanm187@gmail.com>
2019-09-30 06:15:17 +0200

    g4-common: bring back DT2W (squashed)
    https://github.com/aoleary/device_lge_g4-common/commit/cb5b28df31d783bf3516d8e0e2cd2efcbfc80215 Bring back DT2W => g4-common: Overlay: Add double tap to wake node
    https://github.com/aoleary/device_lge_g4-common/commit/47d37ddf7a93e452e27d54e378685f5ab51c4a93 Bring back DT2W => g4-common: power hal: Add back TARGET_TAP_TO_WAKE_NODE
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,6 +185,9 @@ WITH_LINEAGE_CHARGER := false
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_CHARGER_ENABLE_SUSPEND := false
 BACKLIGHT_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/tap2wake"
+TARGET_HAS_NO_WLAN_STATS := true
+TARGET_HAS_NO_POWER_STATS := true
 TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
 
 # QCOM
