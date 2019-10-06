f858ef8046acf137d1c82a11907e1bc3f0de1ca6
Han Wang <416810799@qq.com>
2019-10-06 20:19:15 +0200

    g4-common: Switch to 1.2 version of qcom power HAL

--- g4.mk
+++ g4.mk
@@ -308,7 +308,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service \
-    android.hardware.power@1.1-service-qti \
+    android.hardware.power@1.2-service-qti \
     power.msm8992
 
 # RIL
