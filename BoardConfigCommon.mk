6fd85fd1ffa2491fb83d71c5cf4bc62ab1431ad1
Alessandro Astone <ales.astone@gmail.com>
2019-10-13 09:06:17 +0200

    g4-common: [DNM] disable sepolicy
    * Leave the "SELINUX_IGNORE_NEVERALLOWS := true" flag activated to avoid neverallows build errors.
    
    Co-authored-by: aoleary <aoleary@gmail.com>
    Co-authored-by: steadfasterX <steadfasterX@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -199,10 +199,10 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /vendor/bin/hw/rild=27
 
 # Sepolicy
-include device/qcom/sepolicy/SEPolicy.mk
-BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
+#include device/qcom/sepolicy/SEPolicy.mk
+#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+#BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 SELINUX_IGNORE_NEVERALLOWS := true
 
 # Shims
