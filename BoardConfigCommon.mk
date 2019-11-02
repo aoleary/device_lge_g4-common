4d7e86aa4c8297f800dbe4594d97bb1c80838136
steadfasterX <steadfasterX@gmail.com>
2019-11-02 22:06:27 +0100

    g4-common: moving AAPT stuff to g4 tree

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,10 +206,6 @@ BOARD_PROVIDES_LIBRIL := true
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /vendor/bin/hw/rild=27
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi 560dpi xxxhdpi
-PRODUCT_AAPT_PREF_CONFIG := 560dpi
-
 # Sepolicy
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
