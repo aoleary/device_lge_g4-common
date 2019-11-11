dfb64f19672aad22a25cbd07cc4a101028710f33
steadfasterX <steadfasterX@gmail.com>
2019-11-11 20:15:16 +0100

    g4-common: disable fdroid for the mom

--- g4.mk
+++ g4.mk
@@ -133,10 +133,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
 # F-droid
-WITH_FDROID := true
-PRODUCT_PACKAGES += \
-    F-Droid \
-    FDroidPrivilegedExtension
+#WITH_FDROID := true
+#PRODUCT_PACKAGES += \
+#    F-Droid \
+#    FDroidPrivilegedExtension
 
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(LOCAL_PATH)/config.fs
