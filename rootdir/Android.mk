9dd4740fa4241d76086ecdcabc4b1aa8526c738e
steadfasterX <steadfasterX@gmail.com>
2019-11-11 20:19:32 +0100

    g4-common: init: correct ueventd path

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,7 +40,7 @@ LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
