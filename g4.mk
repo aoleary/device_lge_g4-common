3cd957ad1be28201403fd940b8e83fa9f6a83779
aoleary <seanm187@gmail.com>
2020-04-03 14:43:34 +0000

    Revert "g4: Add and build camera wrapper"
    Remove OSS libs introduced in this commit that we do not use..
    
    This reverts commit 07d4027f0c7462dc39a0180905fa0ef227681515.

--- g4.mk
+++ g4.mk
@@ -66,8 +66,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
-    libqomx_core \
-    libmm-qcamera
+    libqomx_core
 
 # rootless torch tile workaround
 PRODUCT_PACKAGES += \
