e5234a6a8682fe66b5deff55fae4cfada3e6d753
Aleksander <johnnylittleplanet@gmail.com>
2020-04-28 17:49:52 +0200

    g4-common: Binderize Keymaster HIDL
    Change-Id: If1003d6b128a8d37fd6a845b307f45af946a8adf

--- g4.mk
+++ g4.mk
@@ -249,7 +249,8 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
