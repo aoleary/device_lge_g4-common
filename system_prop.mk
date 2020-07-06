5f43a5606cbabc2ac73131354e7d38cfdff1e795
steadfasterX <steadfasterX@gmail.com>
2020-07-06 13:24:07 +0200

    g4-common: props: optimize dalvik heap
    the current size is unreasonable high which wastes a lot of RAM.
    same for the other values.
    
    sources:
    
     * stock nougat build.prop
     * https://www.droidviews.com/improve-androids-performance-by-tweaking-the-vm-heap-size/
     * https://androidforums.com/threads/dalvik-vm-fully-100-explained-how-to-improve-memory-usage.1309359/
     * https://stackoverflow.com/questions/20011424/query-on-properties-in-system-build-prop#20055565
    
    Change-Id: If7c968ccbd4661bca116158bf410095c4f2635ef

--- system_prop.mk
+++ system_prop.mk
@@ -59,11 +59,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=288m \
-    dalvik.vm.heapsize=768m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.50 \
+    dalvik.vm.heapminfree=1m \
     dalvik.vm.heapmaxfree=8m
 
 # Dexopt (try not to use big cores during dexopt)
