74cd280a317c942522771b71f24ea7edc276736c
pavlaras <theodoroupaulos@gmail.com>
2020-07-29 10:54:55 +0300

    g4-common: Call and build external camera provider hal
    Change-Id: Id9cfe96a3ba409ca4d1922bc4b843810fd0bd9f1

--- g4.mk
+++ g4.mk
@@ -75,6 +75,8 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-external \
+    android.hardware.camera.provider@2.4-external-service \
     libqomx_core \
     libmm-qcamera
 
