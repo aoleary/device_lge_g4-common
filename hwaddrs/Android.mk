5cec0d6dd2475da54ddfd61c2013dd9b32aae4ee
steadfasterX <steadfasterX@gmail.com>
2020-09-01 19:21:30 +0200

    g4: hwaddrs: fix name, add ALOG support
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- hwaddrs/Android.mk
+++ hwaddrs/Android.mk
@@ -17,9 +17,10 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := getmac.c
+LOCAL_SRC_FILES := hwaddrs.c
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE := hwaddrs
 LOCAL_PROPRIETARY_MODULE := true
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_EXECUTABLE)
