a8338fa96af2c8d348a045299bc0c87467862615
Arian <arian.kulmer@web.de>
2020-09-22 20:06:14 +0200

    g4-common: Build new AIDL QTI power HAL
    Change-Id: I4bdea354e54d34d0dfe9b811e228fc6f8762d2ed

--- g4.mk
+++ g4.mk
@@ -343,9 +343,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-    #android.hardware.power@1.1-service-qti: this is the Power HAL service
-    #used for our in-built Power HAL
+    android.hardware.power-service-qti
 
 # RIL
 PRODUCT_PACKAGES += \
