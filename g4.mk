c0b4ceb3851132c7c2d4ca85af977e6e4b060604
aoleary <seanm187@gmail.com>
2020-10-15 06:59:32 +0000

    g4-common: remove LiveDisplay

--- g4.mk
+++ g4.mk
@@ -228,11 +228,6 @@ PRODUCT_PACKAGES += \
 	VisualizationWallpapers \
 	librs_jni
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm \
-    vendor.lineage.livedisplay@2.0-service-legacymm
-
 # MP-CTL
 PRODUCT_PACKAGES += \
     libqti-perfd \
