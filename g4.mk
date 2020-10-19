a2077faba58e16a48068b402869d456be017f12c
Kyle Harrison <khwebmail@gmail.com>
2020-10-19 12:52:48 +0100

    g4-common: sensors: Switch android.hardware.sensors@1.0 to passthrough
    Change-Id: Ibada0915c00c9417f947c0d4ff36769c2ba4f396

--- g4.mk
+++ g4.mk
@@ -439,8 +439,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_common.conf:system/etc/sensors/sensor_def_common.conf \
