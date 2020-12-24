c6afd4cd8ae007176d98c6076958b21e236c48e2
Terence Tan <~@calroth.net>
2020-12-24 18:11:39 +1100

    g4-common: libril: compat with LG H815 v29a radio
    In the LG H815 v29a radio stack, LG made some changes to its libril in
    order to interoperate with its libril-qc-qmi-1. These changes include
    extra fields in data structures and extra data returned in calls.
    
    We use the standard AOSP libril for maintainability purposes, but we use
    LG's libril-qc-qmi-1 as this works best with LG's modem firmware.
    
    We have reverse engineered some of LG's changes, and this commit applies
    them to our AOSP libril.

--- libril/ril_service.cpp
+++ libril/ril_service.cpp
@@ -3861,7 +3861,7 @@ int radio::getDataRegistrationStateResponse(int slotId,
             if (e == RIL_E_SUCCESS) responseInfo.error = RadioError::INVALID_RESPONSE;
         } else if (s_vendorFunctions->version <= 14) {
             int numStrings = responseLen / sizeof(char *);
-            if ((numStrings != 6) && (numStrings != 11)) {
+            if ((numStrings != 6) && (numStrings < 11)) {
                 RLOGE("getDataRegistrationStateResponse Invalid response: NULL");
                 if (e == RIL_E_SUCCESS) responseInfo.error = RadioError::INVALID_RESPONSE;
             } else {
