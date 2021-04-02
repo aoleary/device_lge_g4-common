e1bf6b4ebe18b64ced12265cb8f067a443d57df2
aoleary <seanm187@gmail.com>
2021-04-02 07:27:31 +0000

    g4-common: add ro.vold.umsdirtyratio property

--- system_prop.mk
+++ system_prop.mk
@@ -164,13 +164,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dpm.feature=1 \
     persist.timed.enable=true \
     ro.qualcomm.cabl=2 \
     ro.qualcomm.perf.cores_online=2 \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.dpm.feature=1 \
     ro.min_freq_0=384000 \
-    ro.min_freq_4=384000
+    ro.min_freq_4=384000 \
+    ro.vold.umsdirtyratio=50
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
