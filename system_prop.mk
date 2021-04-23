11aa0ae9cfe7c7d70a58c51ebbecf08da702bec7
aoleary <seanm187@gmail.com>
2021-04-23 08:08:04 +0000

    g4-common: Improve download/upload speed
    Improve internet speed by increasing the TCP’s buffersize.
    Also, forcing the device to use Google’s DNS could unlock more speed.
    
    Taken from here: https://www.rankred.com/android-build-prop-tweaks/

--- system_prop.mk
+++ system_prop.mk
@@ -130,6 +130,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.izat.premium_enabled=0 \
     ro.qc.sdk.izat.service_mask=0x0
 
+#INTERNET: improve download/upload performance
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tcp.buffersize.default=4096,87380,256960,4096, 16384,256960 \
+    net.tcp.buffersize.wifi=4096,87380,256960,4096,163 84,256960 \
+    net.tcp.buffersize.umts=4096,87380,256960,4096,163 84,256960 \
+    net.tcp.buffersize.gprs=4096,87380,256960,4096,163 84,256960 \
+    net.tcp.buffersize.edge=4096,87380,256960,4096,163 84,256960 \
+    net.rmnet0.dns1=8.8.8.8 \
+    net.rmnet0.dns2=8.8.4.4 \
+    net.dns1=1.1.1.1\
+    net.dns2=9.9.9.9
+
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.low=1001 \
