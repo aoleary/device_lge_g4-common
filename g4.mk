dea359bf0914828269b1abd504c76a93b46418f2
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    g4-common: Inherit several Android Go configurations
    * Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- g4.mk
+++ g4.mk
@@ -101,6 +101,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     GoogleDialer
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Display
 PRODUCT_PACKAGES += \
