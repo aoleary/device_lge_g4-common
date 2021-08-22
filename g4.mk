e627ba548362192f90adf9cd211c3aa0eb7d2806
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:31:23 +0800

    g4-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- g4.mk
+++ g4.mk
@@ -138,6 +138,8 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
     SystemUI \
     GoogleDialer
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Display
 PRODUCT_PACKAGES += \
