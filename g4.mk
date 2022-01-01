85b68f87272743dc8507bd229e5508d4ae0ea3c7
aoleary <seanm187@gmail.com>
2022-01-01 19:31:32 +0000

    Revert "g4-common: hidl: downgrade radio to 1.0"
    This reverts commit e8ad75fd0d91300b548bc7c42a331582f7c6275a.

--- g4.mk
+++ g4.mk
@@ -348,6 +348,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0-impl \
+    android.hardware.radio.config@1.0-service \
     android.hardware.radio.deprecated@1.0-impl \
     android.hardware.radio.deprecated@1.0-service \
     librmnetctl \
