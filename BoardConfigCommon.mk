c04fb669e93f704846bc2c162b832df27da0d273
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    g4-common: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,6 +146,8 @@ PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/msm8994/display
 PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/msm8994/media
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # HWUI
