a2c3264f077ef4eb80757e8bae209e68a6ce8c36
TARKZiM <tom8476oo@gmail.com>
2022-05-03 09:51:16 +0000

    g4-common: Add LOCAL_MODULE_STEM for ueventd
     * This was stopping us from booting, bruh

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -37,6 +37,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
