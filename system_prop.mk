1972325377716537322d99e02f83639b0c0c2f26
Jiakai Zhang <jiakaiz@google.com>
2022-05-11 16:49:38 +0100

    g4-common: Set System UI compiler filter to speed
    Bug: 227310505
    Test: m
    Change-Id: Ieaa417a5024fd367543ff9879e5d57f0eb41aa94
    (cherry picked from commit eb3d4caa9974932abca7e280138c68e8af7b7aed)

--- system_prop.mk
+++ system_prop.mk
@@ -98,8 +98,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     pm.dexopt.bg-dexopt=everything \
     pm.dexopt.boot=verify \
     pm.dexopt.install=quicken \
-    dalvik.vm.image-dex2oat-filter=speed \
-    ro.vendor.qti.am.reschedule_service=true
+    ro.vendor.qti.am.reschedule_service=true \
+    dalvik.vm.systemuicompilerfilter=speed
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
