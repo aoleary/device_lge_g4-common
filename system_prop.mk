ab58b97854425462372aeff31a8cc9e7644836d1
Vivekachooz <vivekachooz@gmail.com>
2022-07-07 00:24:56 +0530

    g4-common: Set ro.vendor.qti.sys.fw.bg_apps_limit
    Change-Id: Ia0ce89dad5e1d843d442f64bed9a7c9e48c8676c

--- system_prop.mk
+++ system_prop.mk
@@ -199,6 +199,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.min_freq_0=384000 \
     ro.min_freq_4=384000 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=32 \
     ro.vold.umsdirtyratio=50
 
 # Properties for Surfaceflinger
