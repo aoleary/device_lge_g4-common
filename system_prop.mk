7e2c622e634b57bfe9693ae631f511b3e313a431
SamarV-121 <samarvispute121@pm.me>
2022-08-26 11:19:01 +0200

    g4-common: Disable continuous transaction tracing on all build types

--- system_prop.mk
+++ system_prop.mk
@@ -251,7 +251,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    debug.sf.enable_transaction_tracing=false
 
 #Properties to improve gaming experiance
 PRODUCT_PROPERTY_OVERRIDES += \
