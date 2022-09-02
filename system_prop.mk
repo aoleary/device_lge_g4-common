09539fe7360220553a00bb826ca0e9321863c05d
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-02 13:59:04 +0200

    g4-common: Stop using USAP
    * Proven to be detrimental unless you're the cream of the crop SOC.

--- system_prop.mk
+++ system_prop.mk
@@ -297,7 +297,3 @@ persist.service.pcsync.enable=0
 # WiFi Scan Interval (default = 15s)
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.supplicant_scan_interval=600
-
-# Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.device_config.runtime_native.usap_pool_enabled=true
