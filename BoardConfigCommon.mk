ce99163c62a9c19d19bdbd476366d5e0d57828bc
aoleary <seanm187@gmail.com>
2023-01-11 10:35:18 +0000

    g4-common: sepolicy: [SQUASH] revert some sepolicy removals
    Revert "g4-common: sepolicy: remove sepolicies for A12"
    
    This reverts commit 2d60accef56dc5c3bf032a68c9edd16e97b6c3e0.
    
    Revert "g4-common: sepolicy: deactivate sepolicies that A12 doesnt like"
    
    This reverts commit eae740d66ef98e61e85e49bdb31c65b660a1fc54.
    
    Revert "g4-common: [TMP] temporary sepolicy for extra root dirs"
    
    This reverts commit a619f97260bf7dde9a930ec396ac306cfc64479e.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,9 +220,6 @@ PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor/private
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 SELINUX_IGNORE_NEVERALLOWS := true
 
-# Sepolicy-tmp
-BOARD_SEPOLICY_DIRS += device/lge/g4-common/sepolicy-tmp
-
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libwvm.so|libshims_wvm.so \
