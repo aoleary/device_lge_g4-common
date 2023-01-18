236a2ae33d3a5439262685e9bb4d2d712eebf55b
Tommaso Fonda <fondatommaso2@gmail.com>
2023-01-18 16:39:37 +0100

    g4-common: Use bluetooth.audio@2.1
    * bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- g4.mk
+++ g4.mk
@@ -77,7 +77,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     libbt-vendor \
     hwaddrs \
