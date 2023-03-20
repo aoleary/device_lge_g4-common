0873c3e9e1a426ed539f28236c5d5b120778c2ac
Vivekachooz <vivekachooz@gmail.com>
2023-03-20 02:56:51 +0000

    g4-common: Reduce ro.vendor.qti.sys.fw.bg_apps_limit from 32 to 16
    Change-Id: Ief7f7e460b2ea40985ef9bca3d763d820c978d5c

--- system_prop.mk
+++ system_prop.mk
@@ -215,7 +215,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.min_freq_0=384000 \
     ro.min_freq_4=384000 \
-    ro.vendor.qti.sys.fw.bg_apps_limit=32 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=16 \
     ro.vold.umsdirtyratio=50 \
     vendor.perf.gestureflingboost.enable=true
 
