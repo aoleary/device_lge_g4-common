8f9e801cd2860958747229bf764351e46e12fed7
onettboots <blackcocopet@gmail.com>
2023-04-04 22:24:57 +0000

    g4-common: Drop mock power stats
    * E/android.hardware.power.stats@1.0-service-mock(787): Failed to getEnergyData
    
    Signed-off-by: onettboots <blackcocopet@gmail.com>

--- g4.mk
+++ g4.mk
@@ -373,8 +373,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.0 \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.0
 
 # RIL
 PRODUCT_PACKAGES += \
