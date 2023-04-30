6c2d848cb65dc2774e900b9af9dc35f8504852b6
qwertyuiii-code <soorprp@ya.ru>
2023-04-30 17:38:09 +0530

    g4-common: props: Import more surface flinger flags
     - For better smoothness and to eliminate lags
     - QS animation, app opening & app switching seem to have improved.

--- system_prop.mk
+++ system_prop.mk
@@ -248,7 +248,11 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.renderengine.backend=skiaglthreaded \
     debug.hwui.skia_atrace_enabled=false \
     ro.vendor.perf.scroll_opt=true \
-    ro.hwui.render_ahead=1
+    ro.hwui.render_ahead=1 \
+    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 #Properties to improve gaming experiance
 PRODUCT_PROPERTY_OVERRIDES += \
