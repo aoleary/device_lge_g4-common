673030db4f03a347fb9efc2efc23d77cdefba740
aoleary <seanm187@gmail.com>
2023-05-03 09:01:47 +0000

    Revert "g4-common: dont build QTI power hal"
    * removing the QTI power HAL breaks DT2W!
    
    This reverts commit eda0d4e676f77d723a99b5afa19f38ab2684c9dd.

--- g4.mk
+++ g4.mk
@@ -379,7 +379,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
+    android.hardware.power-service-qti \
     android.hardware.power@1.0 \
     android.hardware.power.stats@1.0-service.mock
 
