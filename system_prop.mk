0df5628bf58d347a04b23ca4306af3b481cea6de
ExactExampl <64069095+ExactExampl@users.noreply.github.com>
2023-05-24 18:29:38 +0300

    g4-common: Drop Disable SF client composition cache prop
    * There`s no urgent need to disable SF client composition cache and thus
    * sacrifice potential performance benefit in mixed composition scenarios
    
    This reverts commit a70328bfb916394ac4b2c86bdf0fb729e4ffac29.

--- system_prop.mk
+++ system_prop.mk
@@ -215,7 +215,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.recomputecrop=0 \
     debug.sf.disable_backpressure=1 \
-    debug.sf.disable_client_composition_cache=1 \
     debug.sf.latch_unsignaled=0 \
     dev.pm.dyn_samplingrate=1 \
     hw3d.force=1 \
