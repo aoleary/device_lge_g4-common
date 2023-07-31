e44713b290cea25e72cb367b23638b7e7356d4af
aoleary <seanm187@gmail.com>
2023-07-31 09:53:05 +0000

    Revert "g4-common: Fix bluetooth audio / pair"
    This reverts commit 7f5acb0b7b3770bdd7e241eec92e384e8b83e911.

--- system_prop.mk
+++ system_prop.mk
@@ -40,23 +40,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.chip.vendor=brcm \
     qcom.bluetooth.soc=rome \
     ro.bt.bdaddr_path="/data/misc/bluetooth/bdaddr" \
-    persist.bt.enableAptXHD=true \
-    vendor.audio.offload.gapless.enabled=true \
-    bluetooth.device.class_of_device=90,2,12 \
-    bluetooth.profile.a2dp.source.enabled=true \
-    bluetooth.profile.asha.central.enabled=true \
-    bluetooth.profile.avrcp.target.enabled=true \
-    bluetooth.profile.bas.client.enabled=true \
-    bluetooth.profile.gatt.enabled=true \
-    bluetooth.profile.hfp.ag.enabled=true \
-    bluetooth.profile.hid.device.enabled=true \
-    bluetooth.profile.hid.host.enabled=true \
-    bluetooth.profile.map.server.enabled=true \
-    bluetooth.profile.opp.enabled=true \
-    bluetooth.profile.pan.nap.enabled=true \
-    bluetooth.profile.pan.panu.enabled=true \
-    bluetooth.profile.pbap.server.enabled=true \
-    bluetooth.profile.sap.server.enabled=true
+    persist.bt.enableAptXHD=true
 
 # Blur - Diable Blur in A12+
 ro.surface_flinger.supports_background_blur=0
