c0408cb56f77fe207a865b64614ac5e3cedb01cc
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-18 11:43:40 +0700

    g4-common: Tune dalvik heapminfree for devices with 3gb ram
    * Also correct insanely bad config for dalvik.vm.heapminfreedalvik.vm.heapminfree
    * It was set to 512MB!!!
    
    Signed-off-by: Assunzain <asepsunanda.zain@gmail.com>
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -80,10 +80,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512m \
+    dalvik.vm.heaptargetutilization=0.7 \
+    dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.dex2oat64.enabled=true
 
