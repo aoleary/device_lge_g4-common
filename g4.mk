cd6bf90eeb23b5491a40e3c80b6df9f709192610
aoleary <seanm187@gmail.com>
2023-09-01 12:58:47 +0000

    g4-common: remove reference to extendedrom, since its configured in g4 tree

--- g4.mk
+++ g4.mk
@@ -496,6 +496,3 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:/system/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
-
-# Enable extendrom
-$(call inherit-product-if-exists, vendor/extendrom/config/common.mk)
