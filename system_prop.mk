2c63007912c162501fcdb480b7dc2c2da38c86b5
Long Ling <longling@google.com>
2023-09-27 21:36:20 +0300

    g4-common: Increase active layer timeout to 1s
    * Avoid SF cache thrashing in some cases.
    
    Bug: 189928400
    Test: no flicking issue with the testbuld (removed displaycolor fix)
          and flicking happens after set the prop to 0

--- system_prop.mk
+++ system_prop.mk
@@ -201,7 +201,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=11600000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=11600000 \
+    debug.sf.layer_caching_active_layer_timeout_ms=1000 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
