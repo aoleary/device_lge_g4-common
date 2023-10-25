16f7a2378ca1716fd49af74630e78d2936f89c0e
aoleary <seanm187@gmail.com>
2023-10-25 09:37:02 +0000

    Revert "g4-common: Enable TARGET_HAS_HDR_DISPLAY and TARGET_HAS_WIDE_COLOR_DISPLAY"
    * this seems to cause VSYNC issues as indicated by a user..
    
    This reverts commit c237034b534ac0e72b77456b55c94787729303bf.

--- system_prop.mk
+++ system_prop.mk
@@ -76,8 +76,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Configstore
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.has_wide_color_display=true
-    ro.surface_flinger.has_HDR_display=true
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=2048 \
     ro.surface_flinger.use_color_management=true
