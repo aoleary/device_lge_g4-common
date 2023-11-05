1841fea12b0664d45aef7e123fc63fae90dd8608
Yaroslav Zviezda <acroreiser@gmail.com>
2023-11-05 20:29:14 +0300

    g4-common: set_tasks: move some daemons to background cpuset

--- rootdir/etc/set_tasks.sh
+++ rootdir/etc/set_tasks.sh
@@ -3,8 +3,17 @@
 # move cpu-hungry kswapd and mmcqd/0 tasks to background cpuset
 echo $(pgrep kswapd) > /dev/cpuset/background/tasks
 echo $(pgrep mmcqd/0 | head -n 1) > /dev/cpuset/background/tasks
+echo $(pgrep msm_watchdog) > /dev/cpuset/background/tasks
+echo $(pgrep irqbalance) > /dev/cpuset/background/tasks
+echo $(pgrep rild | head -n 1) > /dev/cpuset/background/tasks
+echo $(pgrep rild | head -n 2 | sed /$(head -n1)/d) > /dev/cpuset/background/tasks
+echo $(pgrep qmuxd) > /dev/cpuset/background/tasks
+echo $(pgrep netmgrd) > /dev/cpuset/background/tasks
+echo $(pgrep logd) > /dev/cpuset/background/tasks
+echo $(pgrep wcnss_service) > /dev/cpuset/background/tasks
 
 # move UI-related threads to top-app cpuset
+echo $(pgrep composer) > /dev/cpuset/top-app/tasks
 echo $(pgrep mdss_dsi_event) > /dev/cpuset/top-app/tasks
 echo $(pgrep kgsl_worker_thr) > /dev/cpuset/top-app/tasks
 
