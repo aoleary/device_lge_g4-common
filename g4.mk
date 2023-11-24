485d8e962f3e08439e1d92ca710f09742e056ffe
aoleary <seanm187@gmail.com>
2023-11-24 16:01:20 +0000

    g4-common: add android.hardware.sensors@2.0 and android.hardware.sensors@2.1
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.sensors@2.1::ISensors/default in either framework or device VINTF manifest.
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.sensors@2.0::ISensors/default in either framework or device VINTF manifest.

--- g4.mk
+++ g4.mk
@@ -440,7 +440,9 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
-    android.hardware.sensors@1.0-impl
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/sensor_def_common.conf:system/etc/sensors/sensor_def_common.conf \
