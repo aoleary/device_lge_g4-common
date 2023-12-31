a8fe950c6cda112fe3bf73df5bbdc3f691c4f9fd
me_cafebabe <me_cafebabe@hotmail.com>
2023-12-31 23:22:51 +0700

    g4-common: Switch to QTI Vibrator AIDL
    It provides haptic effects with qti haptics driver,
    Meanwhile still supports simple vibration with qpnp haptics driver.
    
    Co-authored-by: Roman Rihter <teledurak@gmail.com>
    Change-Id: I83d0c5f562c855afba563e7b959d3e0cd697ef26

--- g4.mk
+++ g4.mk
@@ -512,7 +512,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    vendor.qti.hardware.vibrator.service \
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # ViPER4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
