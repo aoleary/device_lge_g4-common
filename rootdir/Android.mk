737ca281538013ac66552faae10500626e38f2a9
aoleary <seanm187@gmail.com>
2024-01-25 14:50:48 +0000

    g4-common: setup zRam using shell script
    * zRam size now depend on size of device RAM and equals % of TotalRam
    * set it to 25%, as the G4 doesnt use a massive amount, usually less than 512GB
    
    * remove some duplicated entries

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -173,3 +173,11 @@ LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := etc/set_tasks.sh
 LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := set_zram.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := etc/set_zram.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
