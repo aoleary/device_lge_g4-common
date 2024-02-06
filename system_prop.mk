e4715ecbdc02ed64cf4412be0e5107fa2dc66e42
helliscloser <mrumais@gmail.com>
2024-02-06 04:56:04 +0000

    g4-common: Explicitly disable "Enable GL comp backpressure"
    * There is severe lag in; Launcher3 while scrolling apps in App drawer, Scrolling through recents and occasionally on QS pull down.
    
    * Enabling or completely removing the prop here https://github.com/sm6150-davinci/device_xiaomi_sm6150-common/commit/d6233e0e7c6aff352f3c1e7461cce28f90531faf doesn't seem to make an effect.
    * Explicitly disabling the "Enable GL comp backpressure" prop seems to slove the issue.
    
    * Lag & janks are gone now, even with blur.
    
    Change-Id: I2ffc9c18fe7b6053e9d39ebbb50221b77f0607a5
    Signed-off-by: helliscloser <mrumais@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -242,6 +242,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.start_graphics_allocator_service=true \
     ro.surface_flinger.use_context_priority=true \
     debug.sf.enable_planner_prediction=false \
+    debug.sf.enable_gl_backpressure=0 \
     ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
