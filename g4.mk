9a67bd06e0790dc8ebc39b4834e23c6d19957e9c
steadfasterX <steadfasterX@gmail.com>
2024-02-27 07:50:47 +0000

    g4-common: [SQUASH] re-enable disk encryption
    This reverts commit d9164613d376657ec91407853237ef6dff34370e.

--- g4.mk
+++ g4.mk
@@ -128,6 +128,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# CryptfsHW
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Dex - Pre-opt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
