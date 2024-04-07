d04b3629a1c8de5e6d2a2a19b5aaf5dba84e5e91
aoleary <seanm187@gmail.com>
2024-04-07 07:19:45 +0000

    g4-common: switch to schedutil cpu governor(2/2)

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -76,46 +76,18 @@ case "$target" in
 # Big: 384000 480000 633600 768000 864000 960000 1248000 1344000 1440000 1536000 1632000 1689600 1824000
 
 # configure governor settings for little cluster
-	echo impulse > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+	echo schedutil > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 	echo 1440000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq		#Core 4 Maximum Frequency = 1440MHz
-	echo "80 384000:33 460800:25 600000:50 672000:65 787200:70 864000:85 960000:90 1248000:92 1440000:98" > /sys/devices/system/cpu/cpu0/cpufreq/impulse/target_loads #Set normal max frequency target_loads
-
-#Tweak Impulse Governor
-	echo 85 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/go_hispeed_load
-	echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/above_hispeed_delay
-	echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/timer_rate
-	echo 960000 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/hispeed_freq
-	echo -1 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/timer_slack
-	echo 50000 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/min_sample_time
-	echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/powersave_bias
-	echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/align_windows
-	echo 166667 > /sys/devices/system/cpu/cpu0/cpufreq/impulse/max_freq_hysteresis
 
 # online CPU4
         write /sys/devices/system/cpu/cpu4/online 1
 
 # configure governor settings for big cluster
-	echo interactive > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+	echo schedutil > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
 	echo 1824000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq		#Core 4 Maximum Frequency = 1824MHz
-	echo "74 768000:73 864000:64 960000:80 1248000:61 1344000:69 1440000:64 1536000:74 1632000:69 1689600:67 1824000:72" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads #Set normal max frequency target_loads
-
 
 # restore A57's max
         cat /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_max_freq /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
-
-	#Tweak interactive Governor
-	echo 90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
-	echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-	echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
-	echo 1440000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
-	echo -1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_slack
-	echo 30000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
-	echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/boost
-	echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/align_windows
-	echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
-	echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
-	echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
-	echo 80000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/boostpulse_duration
 
 # plugin remaining A57s
         echo 1 > /sys/devices/system/cpu/cpu5/online
@@ -123,9 +95,9 @@ case "$target" in
 #Enable Input Boost for LITTLE cluster @600MHz for 40ms
 	echo Enabling Input Boost at 600 MHz for the LITTLE cluster
 	echo 1 > /sys/module/cpu_boost/parameters/input_boost_enabled
-	echo "0:787200 1:787200 2:787200 3:787200 4:0 5:0" > /sys/module/cpu_boost/parameters/input_boost_freq
+	echo "0:600000 1:600000 2:600000 3:600000 4:0 5:0" > /sys/module/cpu_boost/parameters/input_boost_freq
 	echo 0 > /sys/module/cpu_boost/parameters/boost_ms
-	echo 80 > /sys/module/cpu_boost/parameters/input_boost_ms
+	echo 100 > /sys/module/cpu_boost/parameters/input_boost_ms
 
 # GPU Input Boost
 # Available CPU Freqs in kernel
