5e0e5e940ec96a3a12b715a4750c4aa105ef2619
SonicBSV <sonicbsv@gmail.com>
2024-04-15 20:02:35 +0300

    g4-common: doze: Migrate to CompoundButton.OnCheckedChangeListener
    device/lge/g4-common/doze/src/org/lineageos/settings/doze/DozeSettingsActivity.java:23: error: cannot find symbol
    import com.android.settingslib.widget.R;
                                         ^
      symbol:   class R
      location: package com.android.settingslib.widget
    device/lge/g4-common/doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java:36: error: cannot find symbol
    import com.android.settingslib.widget.OnMainSwitchChangeListener;
                                         ^
      symbol:   class OnMainSwitchChangeListener
      location: package com.android.settingslib.widget
    device/lge/g4-common/doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java:39: error: cannot find symbol
            Preference.OnPreferenceChangeListener, OnMainSwitchChangeListener {
                                                   ^
      symbol: class OnMainSwitchChangeListener
    device/lge/g4-common/doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java:63: error: incompatible types: DozeSettingsFragment cannot be converted to OnCheckedChangeListener
            mSwitchBar.addOnSwitchChangeListener(this);
                                                 ^
    device/lge/g4-common/doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java:98: error: method does not override or implement a method from a supertype
        @Override
        ^
    
    https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/87f8cc1800e5b650ac83cf7e0cf8706a97bd4151

--- doze/src/org/lineageos/settings/doze/DozeSettingsActivity.java
+++ doze/src/org/lineageos/settings/doze/DozeSettingsActivity.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2015-2016 The CyanogenMod Project
- *               2017,2021 The LineageOS Project
+ * Copyright (C) 2018,2021-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +19,6 @@ package org.lineageos.settings.doze;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -30,7 +28,8 @@ public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction()
+        .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new DozeSettingsFragment(), TAG_DOZE).commit();
     }
 }
