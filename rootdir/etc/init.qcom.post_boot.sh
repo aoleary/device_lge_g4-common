dee35b7d3040586a976eab16b03f9931d34b6e47
Yaroslav Zviezda <acroreiser@gmail.com>
2024-05-04 22:07:31 +0300

    g4-common: remove power aware task placement
    * cleanup badly implemented config

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -260,13 +260,6 @@ fi
 # Tune task scheduler to prefer idle cpus to place task
 sysctl -w kernel.sched_prefer_idle=1
 
-# Tune task scheduler to reduce possibility 
-# to pick some heavy task to the same cpu 
-# with ui-threads
-sysctl -w kernel.sched_spill_load=85
-sysctl -w kernel.sched_window_stats_policy=2
-sysctl -w kernel.sched_ravg_hist_size=2
-
 # Limit CPU and IO resources for background tasks
 echo 102 > /dev/cpuctl/background/cpu.shares
 echo 100 > /dev/blkio/background/blkio.weight
