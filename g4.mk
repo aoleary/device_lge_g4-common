6c337f877afe695df20494edfcd4c9f8bbf6d2d9
aoleary <seanm187@gmail.com>
2024-05-25 11:23:49 +0000

    g4-common: build libminijail and libaudioroute for A14
    * No longer build by default
    
    * 01-21 02:40:48.497  1511  1511 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.media.omx@1.0-service":
    * library "libminijail.so" not found: needed by /system/vendor/lib/libavservices_minijail.so in namespace (default)

--- g4.mk
+++ g4.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     libaudio-resampler \
+    libaudioroute \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
@@ -336,7 +337,8 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw \
-    libstagefright_soft_flacdec
+    libstagefright_soft_flacdec \
+    libminijail
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
