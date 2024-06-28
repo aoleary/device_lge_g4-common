7dfc7f8210b37339174e61b0a9aa8ff4bb2a0f00
Mukul Dhir <dhirmukul3@gmail.com>
2024-06-29 01:31:49 +0200

    g4-common: Build android.hardware.bluetooth.audio-impl
    01-01 01:05:58.122  1369  1369 E audiohalservice: Failed to dlopen android.hardware.bluetooth.audio-impl.so: dlopen failed: library "android.hardware.bluetooth.audio-impl.so" not found

--- g4.mk
+++ g4.mk
@@ -80,6 +80,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libbt-vendor \
     hwaddrs \
