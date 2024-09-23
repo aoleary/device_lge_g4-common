e773cfd57d7219016f91c4474f41dfb4692811c8
mukul2259 <dhirmukul3@gmail.com>
2024-09-23 19:02:42 +0200

    g4-common: Switch back to HIDL Vibrator hal
     * Breaks vibration, no kernel side support
     * Also add it to FCM

--- g4.mk
+++ g4.mk
@@ -512,10 +512,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service \
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    android.hardware.vibrator@1.0-impl
 
 # ViPER4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
