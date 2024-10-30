f7fff68a05dc49eba237c819d79182a783cf23f1
Yaroslav Zviezda <acroreiser@gmail.com>
2024-10-30 23:08:05 +0300

    g4-common: drop additional resource limiting for background and dex2oat cgroups

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -217,13 +217,6 @@ echo 1 > /dev/cpuctl/top-app/cpu.uclamp.latency_sensitive
 echo max > /dev/cpuctl/camera-daemon/cpu.uclamp.min
 echo 1 > /dev/cpuctl/camera-daemon/cpu.uclamp.latency_sensitive
 
-# Limit CPU and IO resources for background tasks
-echo 102 > /dev/cpuctl/background/cpu.shares
-echo 100 > /dev/blkio/background/blkio.weight
-
-# Limit resources for dex2oat
-echo 102 > /dev/cpuctl/dex2oat/cpu.shares
-
 # Disable wsf for all targets beacause we are using efk.
 # wsf Range : 1..1000 So set to bare minimum value 1.
 echo 1 > /proc/sys/vm/watermark_scale_factor
