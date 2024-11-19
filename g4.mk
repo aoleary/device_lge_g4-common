ffaed27ab32e02d2f0a8054bc23ffb958db72ac8
aoleary <seanm187@gmail.com>
2024-11-19 09:07:35 +0000

    g4-common: remove duplicate renderscript entryff

--- g4.mk
+++ g4.mk
@@ -430,10 +430,6 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     ro.telephony.default_network=12
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
