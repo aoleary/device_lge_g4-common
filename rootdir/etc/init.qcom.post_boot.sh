7ef6c143990278e6b5cc27d0813bbdda27fff044
Yaroslav Zviezda <acroreiser@gmail.com>
2024-12-23 21:22:11 +0300

    g4-common: do not limit cputime for foreground tasks

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -290,10 +290,6 @@ echo "179:64 4194304" > /dev/blkio/background/blkio.throttle.write_bps_device
 echo 1000000 > /dev/cpuctl/background/cpu.cfs_period_us
 echo 2000000 > /dev/cpuctl/background/cpu.cfs_quota_us
 
-# Limit cputime for foreground tasks
- echo 1000000 > /dev/cpuctl/background/cpu.cfs_period_us
- echo 3000000 > /dev/cpuctl/background/cpu.cfs_quota_us
-
 # Limit cputime for dex2oat
 echo 1000000 > /dev/cpuctl/dex2oat/cpu.cfs_period_us
 echo 2000000 > /dev/cpuctl/dex2oat/cpu.cfs_quota_us
