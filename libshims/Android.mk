43a0bf640e2671be16e7f2c0c0bfb22ccc3ce7db
J0SH1X <aljoshua.hell@gmail.com>
2025-06-21 23:56:59 +0000

    g4-common: libshims: partly convert to bp

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -18,59 +18,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libqsap_shim.c
 LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog
-LOCAL_C_INCLUDES := $(COMMON_PATH)/softap/sdk
+LOCAL_C_INCLUDES := device/lge/g4-common/softap/sdk
 LOCAL_MODULE := libqsap_shim
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
-# cameraclient
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := cameraclient_shim.cpp
-LOCAL_MODULE := libshim_cameraclient
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-# Fence
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := fence.cpp
-LOCAL_MODULE := libfence_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
-# RTP
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := rtp.cpp
-LOCAL_MODULE := ims_rtp_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_CFLAGS_arm64 := -DLIBSHIMS_64BIT
-include $(BUILD_SHARED_LIBRARY)
-
-# Slim_shim
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := slim_shim.cpp
-LOCAL_MODULE := slim_shim
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_VENDOR_MODULE := true
-LOCAL_CFLAGS_arm64 := -DLIBSHIMS_64BIT
-
-# AudioSystem
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libaudioclient_shim.cpp
-LOCAL_MODULE := libaudioclient_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SHARED_LIBRARIES := libaudioclient
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := libshims_thermal.cpp
-LOCAL_CFLAGS := -Wno-unused-parameter
-LOCAL_MODULE := libshims_thermal
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
