887f5dfc2ab1bdb31d7c8ede11a209a87a4ecc36
J0SH1X <aljoshua.hell@gmail.com>
2025-06-22 16:30:11 +0000

    g4-common: remove non existant manifests

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,10 +145,7 @@ PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/msm8992/media
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # HWUI
