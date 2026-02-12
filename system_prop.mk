2c8d609ef5496e63009b27105467da3f3f078029
J0SH1X <aljoshua.hell@gmail.com>
2026-02-12 22:11:37 +0000

    g4-common: start moving props to vendor

--- system_prop.mk
+++ system_prop.mk
@@ -6,42 +6,6 @@ persist.vendor.usb.config=mtp,adb
 # ANR workaround : Increase watchdog timeout multiplier
     ro.hw_timeout_multiplier=6
 
-# Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.audio.sdk.fluencetype=fluencepro \
-    persist.vendor.audio.fluence.audiorec=false \
-    persist.vendor.audio.fluence.speaker=false \
-    persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicecomm=true \
-    persist.vendor.audio.fluence.voicerec=false \
-    persist.speaker.prot.enable=true \
-    ro.config.vc_call_vol_steps=7 \
-    persist.vendor.audio.calfile0=/etc/acdbdata/Bluetooth_cal.acdb \
-    persist.vendor.audio.calfile1=/etc/acdbdata/General_cal.acdb \
-    persist.vendor.audio.calfile2=/etc/acdbdata/Global_cal.acdb \
-    persist.vendor.audio.calfile3=/etc/acdbdata/Handset_cal.acdb \
-    persist.vendor.audio.calfile4=/etc/acdbdata/Hdmi_cal.acdb \
-    persist.vendor.audio.calfile5=/etc/acdbdata/Headset_cal.acdb \
-    persist.vendor.audio.calfile6=/etc/acdbdata/Speaker_cal.acdb \
-    persist.spkr.cal.duration=0 \
-    ro.qc.sdk.audio.ssr=false \
-    persist.audio.ssr.3mic=true \
-    av.offload.enable=false \
-    audio.offload.buffer.size.kb=32 \
-    audio.offload.pcm.16bit.enable=false \
-    audio.offload.gapless.enabled=false \
-    audio.offload.passthrough=false \
-    audio.offload.pcm.24bit.enable=true \
-    audio.offload.multiple.enabled=false \
-    audio.deep_buffer.media=true \
-    tunnel.audio.encode=false \
-    use.dedicated.device.for.voip=true \
-    af.fast_track_multiplier=1 \
-    audio_hal.period_size=192 \
-    ro.audio.flinger_standbytime_ms=300 \
-    use.voice.path.for.pcm.voip=false \
-    ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -75,35 +39,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.bluetooth.a2dp_offload.supported=false \
     persist.bluetooth.a2dp_offload.disabled=true
 
-# Blur - Diable Blur in A12+
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.sf.disable_blurs=1 \
-    ro.surface_flinger.supports_background_blur=0 \
-    ro.sf.blurs_are_expensive=0 \
-    ro.launcher.blur.appLaunch=0
-
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.camera.expose.aux=1 \
-    ro.qc.sdk.camera.facialproc=false \
-    ro.qc.sdk.gestures.camera=false \
-    camera2.portability.force_api=1 \
-    persist.camera.video.ubwc=0 \
-    persist.camera.HAL3.enabled=1 \
-    persist.ts.postmakeup=false \
-    persist.ts.rtmakeup=false \
-    camera.no_navigation_bar=true \
-    ro.factorytest=0 \
-    vidc.debug.perf.mode=2 \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera,com.google.camera \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
-    vendor.camera.aux.packageexcludelist=org.telegram.messenger,com.microsoft.teams,com.discord
-
-# DRM
-PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=true
-
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
@@ -125,32 +60,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.systemuicompilerfilter=speed \
     pm.dexopt.bg-dexopt=everything \
     ro.vendor.qti.am.reschedule_service=true \
-# filter unused apps
     pm.dexopt.downgrade_after_inactive_days=10 \
     pm.dexopt.shared=verify
 
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=560 \
-    qemu.hw.mainkeys=0 \
-    ro.qualcomm.cabl=2 \
-    ro.config.avoid_gfx_accel=true
-# MSM8992 HAL settings
-# 196610 is decimal for 0x30002 to report major/minor versions as 3/2
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.mdpcomp.logs=0 \
-    persist.debug.wfd.enable=1 \
-    persist.demo.hdmirotationlock=false \
-    persist.hwc.enable_vds=1 \
-    persist.hwc.mdpcomp.enable=true \
-    persist.mdpcomp.4k2kSplit=1 \
-    persist.mdpcomp_perfhint=50 \
-    persist.metadata_dynfps.disable=true \
-    persist.sys.ui.hw=1 \
-    persist.sys.wfd.virtual=0 \
-    ro.hardware.egl=adreno \
-    ro.opengles.version=196610 \
-    ro.sf.compbypass.enable=0
 
 # Fling Velocity
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -162,13 +74,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qti.inputopts.movetouchslop=0.6 \
     ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
 
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qc_nlp_in_use=1 \
-    persist.loc.nlp_name=com.qualcomm.services.location \
-    ro.gps.agps_provider=1 \
-    ro.qc.sdk.izat.premium_enabled=0 \
-    ro.qc.sdk.izat.service_mask=0x0
+
 
 # IORapd
 PRODUCT_SYSTEM_PROPERTIES += \
@@ -193,20 +99,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.init_log_level=7
 
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.media.treble_omx=false \
-    persist.mm.enable.prefetch=true \
-    mm.enable.qcom_parser=3379827 \
-    mm.enable.smoothstreaming=true \
-    media.aac_51_output_enabled=true \
-    media.stagefright.legacyencoder=true \
-    media.stagefright.less-secure=true \
-    vidc.debug.level=1 \
-    vidc.debug.perf.mode=2 \
-    vidc.enc.dcvs.extra-buff-count=2 \
-    persist.camera.cpp.duplication=false \
-    ro.config.avoid_gfx_accel=true
+
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -221,94 +114,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vold.umsdirtyratio=50 \
     vendor.perf.gestureflingboost.enable=true
 
-# Properties for Surfaceflinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.max_virtual_display_dimension=2048 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.use_color_management=false \
-    ro.surface_flinger.has_HDR_display=false \
-    ro.surface_flinger.has_wide_color_display=false \
-    ro.surface_flinger.enable_layer_caching=false \
-    ro.surface_flinger.start_graphics_allocator_service=true \
-    ro.surface_flinger.use_context_priority=true \
-    debug.sf.enable_planner_prediction=false \
-    debug.sf.enable_gl_backpressure=0 \
-    ro.surface_flinger.set_idle_timer_ms=80 \
-    ro.surface_flinger.set_touch_timer_ms=200 \
-    ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.support_kernel_idle_timer=true \
-    debug.sf.layer_caching_active_layer_timeout_ms=1000 \
-    debug.cpurend.vsync=false \
-    debug.enable.sglscale=1 \
-    debug.enabletr=true \
-    debug.egl.hw=0 \
-    debug.sdm.support_writeback=0 \
-    debug.sf.latch_unsignaled=0\
-    debug.sf.auto_latch_unsignaled=0 \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.disable_gl_backpressure=1 \
-    debug.sf.predict_hwc_composition_strategy=0 \
-    debug.sf.recomputecrop=0 \
-    debug.sf.use_phase_offsets_as_durations=1 \
-    debug.sf.early.app.duration=13666666 \
-    debug.sf.early.sf.duration=15666666 \
-    debug.sf.earlyGl.app.duration=13666666 \
-    debug.sf.earlyGl.sf.duration=15666666 \
-    debug.sf.late.app.duration=13666666 \
-    debug.sf.late.sf.duration=15666666 \
-    dev.pm.dyn_samplingrate=1 \
-    persist.hwc.ptor.enable=true \
-    persist.sys.composition.type=gpu \
-    ro.fb.mode=1 \
-    video.accelerate.hw=1 \
-    debug.hwui.renderer=opengl \
-    ro.hwui.render_ahead=20 \
-    vendor.display.disable_rotator_downscale=1
 
-# RIL
-PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM \
-    persist.data.mode=concurrent \
-    persist.data.netmgrd.qos.enable=true \
-    persist.data.qmi.adb_logmask=0 \
-    persist.qcril.disable_retry=true \
-    persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.custom_ecc=1 \
-    persist.radio.sib16_support=1 \
-    ro.data.large_tcp_window_size=true \
-    ro.use_data_netmgrd=true \
-    ro.ril.svlte1x=false \
-    ro.ril.svdo=false \
-    persist.telephony.oosisdc=false \
-    ro.ril.enable.amr.wideband=1 \
-    ro.telephony.block_binder_thread_on_incoming_calls=false
-
-# RIL Powersaving
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.add_power_save=1 \
-    pm.sleep_mode=1 \
-    ro.ril.disable.power.collapse=0 \
-    ro.ril.fast.dormancy.rule=1 \
-    ro.ril.fast.dormancy.timeout=3 \
-    ro.mot.eri.losalert.delay=1000
-
-# Sensors
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.sensors.gestures=true \
-    persist.debug.sensors.hal=e \
-    debug.qualcomm.sns.daemon=e \
-    debug.qualcomm.sns.hal=e \
-    debug.qualcomm.sns.libsensor1=e
 
 # Shutdown
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.shutdown_timeout=2
-
-# Storage
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.sdcardfs=true
 
 # UI Smoothening
 PRODUCT_PROPERTY_OVERRIDES += \
