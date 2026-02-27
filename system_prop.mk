62da1d8fe157e249990d4ea711c7a1239330a473
aoleary <seanm187@gmail.com>
2026-02-27 21:16:00 +0000

    g4-common: tune anxiety to work with PSI/Schedutil

--- system_prop.mk
+++ system_prop.mk
@@ -167,11 +167,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Low memory killer daemon
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.lmk.use_psi=true \
-ro.lmk.psi_partial_stall_ms=70 \
-ro.lmk.psi_complete_stall_ms=400 \
-ro.lmk.thrashing_limit=0 \
-ro.lmk.swap_free_low_percentage=20
+   ro.lmk.use_psi=true \
+   ro.lmk.psi_partial_stall_ms=80 \
+   ro.lmk.psi_complete_stall_ms=400 \
+   ro.lmk.thrashing_limit=30 \
+   ro.lmk.thrashing_limit_decay=50 \
+   ro.lmk.swap_free_low_percentage=10
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
