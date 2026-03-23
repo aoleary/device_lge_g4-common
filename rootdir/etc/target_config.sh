af768fd46e90249737b7cd3c8043a69a9ff78be2
aoleary <seanm187@gmail.com>
2026-03-23 13:40:21 +0000

    g4-common: sync uclamp alignment with post_boot.sh

--- rootdir/etc/target_config.sh
+++ rootdir/etc/target_config.sh
@@ -2,8 +2,8 @@
 
 echo 0 > /proc/sys/kernel/sched_util_clamp_min
 
-echo 30 > /proc/sys/kernel/sched_small_task
-echo 0 > /proc/sys/kernel/sched_init_task_load
-echo 15 > /proc/sys/kernel/sched_spill_nr_run
-echo 30 > /proc/sys/kernel/sched_mostly_idle_load
+echo 24 > /proc/sys/kernel/sched_small_task
+echo 15 > /proc/sys/kernel/sched_init_task_load
+echo 3 > /proc/sys/kernel/sched_spill_nr_run
+echo 24 > /proc/sys/kernel/sched_mostly_idle_load
 
