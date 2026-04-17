fcaa7996690317ff057c7feeee477484b4f7c52b
aoleary <seanm187@gmail.com>
2026-04-17 07:30:52 +0000

    g4-common: remove duplicate WCNSS install rule
     * Drop legacy BUILD_PREBUILT for WCNSS_qcom_cfg.ini and keep PRODUCT_COPY_FILES as the single install source.
    
       Also switch to $(TARGET_COPY_OUT_VENDOR) path.
       Fixes duplicate target warnings during build.

--- g4.mk
+++ g4.mk
@@ -518,7 +518,7 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339/device-bcm.mk)
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:/system/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
 
 #Sign builds to pass Play Certification
 -include vendor/lineage-priv/keys/keys.mk
