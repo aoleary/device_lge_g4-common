3d5fe3ef4c5b68e5ada01c9f3e0939bcf50b4774
aoleary <seanm187@gmail.com>
2026-07-17 06:22:36 +0000

    g4-common: remove deprecated RIL props

--- system_prop.mk
+++ system_prop.mk
@@ -155,18 +155,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
 	ro.hwui.text_large_cache_width=2048 \
 	ro.hwui.text_large_cache_height=1024
 
-#INTERNET: improve download/upload performance
-PRODUCT_PROPERTY_OVERRIDES += \
-    net.tcp.buffersize.default=4096,87380,256960,4096,16384,256960 \
-    net.tcp.buffersize.wifi=4096,87380,256960,4096,16384,256960 \
-    net.tcp.buffersize.umts=4096,87380,256960,4096,16384,256960 \
-    net.tcp.buffersize.gprs=4096,87380,256960,4096,16384,256960 \
-    net.tcp.buffersize.edge=4096,87380,256960,4096,16384,256960 \
-    net.rmnet0.dns1=8.8.8.8 \
-    net.rmnet0.dns2=8.8.4.4 \
-    net.dns1=1.1.1.1\
-    net.dns2=9.9.9.9
-
 # Low memory killer daemon
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.lmk.use_psi=true \
@@ -246,30 +234,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM \
-    persist.data.mode=concurrent \
     persist.data.netmgrd.qos.enable=true \
-    persist.data.qmi.adb_logmask=0 \
     persist.qcril.disable_retry=true \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.custom_ecc=1 \
     persist.radio.sib16_support=1 \
-    ro.data.large_tcp_window_size=true \
     ro.use_data_netmgrd=true \
-    ro.ril.svlte1x=false \
-    ro.ril.svdo=false \
-    persist.telephony.oosisdc=false \
-    #Improve Speech Quality
-    ro.ril.enable.amr.wideband=1
-
-# RIL Powersaving
-PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.enable.amr.wideband=1 \
     persist.radio.add_power_save=1 \
-    pm.sleep_mode=1 \
-    ro.ril.disable.power.collapse=0 \
-    ro.ril.fast.dormancy.rule=1 \
-    ro.ril.fast.dormancy.timeout=3 \
-    ro.mot.eri.losalert.delay=1000
+    ro.ril.disable.power.collapse=0
 
 # Security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -291,10 +264,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
    persist.service.lgospd.enable=0 \
    persist.service.pcsync.enable=0
-
-# WiFi Scan Interval (default = 15s)
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.supplicant_scan_interval=600
 
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
